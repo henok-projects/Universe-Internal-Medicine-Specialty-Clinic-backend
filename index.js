88cd2b11ab02d8e0468b8e5326c52237b04abede
henok-projects <45355566+henok-projects@users.noreply.github.com>
2024-12-15 23:26:51 +0300

    Update index.js
    Configure CORS

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://universe-internal-medicine-specialty-clinic-xvy6.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  })
+);
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
